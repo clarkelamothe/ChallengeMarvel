b0e3453fede8266bb2037c8dcf9508d714b20a2c
Clarke Lamothe <clarkelamothe@yahoo.fr>
2021-10-25 00:12:59 -0300

    fun to parse year added

--- app/src/main/java/com/clarkelamothe/intermedia/utils/Utils.kt
+++ app/src/main/java/com/clarkelamothe/intermedia/utils/Utils.kt
@@ -2,3 +2,6 @@ package com.clarkelamothe.intermedia.utils
 
 fun imageUrl(path: String, extension: String): String =
     "$path/standard_large.$extension".replace("http", "https")
+
+
+fun parseYear(format: String) = format.substringBefore("-")
