244c53ab7bcdba9bc5a79edd1926d956d4302768
Clarke Lamothe <clarkelamothe@yahoo.fr>
2021-10-25 06:58:50 -0300

    title of characters in toolbar and close button added

--- app/src/main/java/com/clarkelamothe/intermedia/ui/characterDetails/CharacterDetailsFragment.kt
+++ app/src/main/java/com/clarkelamothe/intermedia/ui/characterDetails/CharacterDetailsFragment.kt
@@ -8,8 +8,10 @@ import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.clarkelamothe.intermedia.R
 import com.clarkelamothe.intermedia.data.Resource
 import com.clarkelamothe.intermedia.databinding.FragmentCharacterDetailsBinding
+import com.clarkelamothe.intermedia.ui.MainActivity
 import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -26,7 +28,23 @@ class CharacterDetailsFragment() : Fragment() {
         binding = FragmentCharacterDetailsBinding.inflate(inflater, container, false)
 
         setData()
+        loadComics()
 
+        return binding.root
+    }
+
+    private fun setData() {
+        arguments?.apply {
+            (activity as MainActivity?)!!.setActionBarTitle(get("characterName").toString())
+            (activity as MainActivity?)!!.changeBackButton(R.drawable.ic_baseline_close_24)
+
+            binding.characterDetailsDescription.text = get("description").toString()
+            Picasso.get().load(get("imageUrl").toString()).into(binding.characterDetailsImage)
+        }
+        charactersDetailsViewModel.getComicsResponse(arguments?.getInt("id").toString())
+    }
+
+    private fun loadComics() {
         charactersDetailsViewModel.comicResult.observe(viewLifecycleOwner, {
             when (it.status) {
                 Resource.Status.LOADING -> {
@@ -38,18 +56,10 @@ class CharacterDetailsFragment() : Fragment() {
                     binding.rvComics.adapter = charactersDetailsAdapter
                 }
                 Resource.Status.ERROR -> {
-                    Toast.makeText(context, "Oops! Something went wrong.", Toast.LENGTH_LONG).show()
+                    Toast.makeText(context, getString(R.string.error_message), Toast.LENGTH_LONG)
+                        .show()
                 }
             }
         })
-        return binding.root
-    }
-
-    private fun setData() {
-        arguments?.apply {
-            binding.characterDetailsDescription.text = get("description").toString()
-            Picasso.get().load(get("imageUrl").toString()).into(binding.characterDetailsImage)
-        }
-        charactersDetailsViewModel.getComicsResponse(arguments?.getInt("id").toString())
     }
 }
